ec70d7131e4090a8303891ae9c82063a4d1d58d9
Adnana Maria Tudose <adnanamariat@gmail.com>
2022-10-12 16:57:01 +0200

    Update 3_Surgery_Timings.R

--- StatsLab/final_code/3_Surgery_Timings.R
+++ StatsLab/final_code/3_Surgery_Timings.R
@@ -1,5 +1,6 @@
-# LONG FORMAT --------
-# Operations timing of surgeries creation -------
+# LONG FORMAT
+# Operations timing of surgeries creation
+
 library(dplyr)
 library(readr)
 library(lubridate)
@@ -9,10 +10,8 @@ setwd("~/Desktop/Data Folder - StatsLab - May 21")
 my_repo <- "~/Documents/ETH/Stats Lab/"
 source(paste0(my_repo, "polytrauma/code/preprocessing/helper_functions.R"))
 
-# Defining time between surgeries ----------
+# Defining time between surgeries
 time_points_bw_surgeries <- function(operations, starting = "Startdat", ending = "Stopdat") {
-  #browser()
-
   all_surgeries <- operations %>% select(-SurgeryNr) %>% group_by(research_case_id)%>% 
     arrange(.data[[starting]])  %>% mutate(SurgeryNr = row_number()) %>%
     group_by(research_case_id, SurgeryNr) %>% 
@@ -23,18 +22,13 @@ time_points_bw_surgeries <- function(operations, starting = "Startdat", ending =
   all_surgeries
 }
 
-# creation -----------
+# creation
 operations <- read.csv("H_Operationen.csv")
-
-length(unique(operations$research_case_id)) # only 1131 patients? How?
 
 operations <- operations %>% mutate(Startdat = ymd_hms(Startdat, tz = "Europe/Zurich"),
                                     Stopdat = ymd_hms(Stopdat, tz = "Europe/Zurich"))
 
-
 surgery_times <- time_points_bw_surgeries(operations) 
-# writing to file --------
 
-#write.csv(surgery_times, "H_Operationen_Surgery_Times_long.csv", row.names = F)
+# writing to file
 save(surgery_times, file = "H_Operationen_Surgery_Times_long.RData")
-
